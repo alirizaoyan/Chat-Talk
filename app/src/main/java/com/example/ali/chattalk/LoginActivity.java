5832b7329579450d92809ff3aae47c7e16471ab4
Ali Rıza <alirizaoyan@gmail.com>
2018-01-12 12:44:43 +0200

    Ekleme
    Login ve Logout fonksiyonları eklendi.

--- app/src/main/java/com/example/ali/chattalk/LoginActivity.java
+++ app/src/main/java/com/example/ali/chattalk/LoginActivity.java
@@ -17,21 +17,21 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
 
-    private Button buttonRegister;
+    private Button buttonSignin;
     private EditText editTextEmail;
     private EditText editTextPassword;
-    private TextView textViewSignin;
-
-    private ProgressDialog progressDialog;
+    private TextView textViewSignup;
 
     private FirebaseAuth firebaseAuth;
+
+    private ProgressDialog progressDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
 
         firebaseAuth=FirebaseAuth.getInstance();
 
@@ -41,22 +41,20 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             startActivity(new Intent(getApplicationContext(),ProfileActivity.class));
         }
 
+        buttonSignin=(Button) findViewById(R.id.buttonSignin);
+        editTextEmail=(EditText) findViewById(R.id.editTextEmail);
+        editTextPassword=(EditText)findViewById(R.id.editTextPassword);
+        textViewSignup=(TextView)findViewById(R.id.textViewSignUup);
+
         progressDialog = new ProgressDialog(this);
 
-        buttonRegister=(Button) findViewById(R.id.buttonRegister);
-
-        editTextEmail=(EditText) findViewById(R.id.editTextEmail);
-        editTextPassword = (EditText) findViewById(R.id.editTextPassword);
-
-        textViewSignin = (TextView) findViewById(R.id.textViewSignin);
-
-        buttonRegister.setOnClickListener(this);
-        textViewSignin.setOnClickListener(this);
+        buttonSignin.setOnClickListener(this);
+        textViewSignup.setOnClickListener(this);
     }
 
-    private void registerUser(){
-        String email = editTextEmail.getText().toString().trim();
-        String password = editTextPassword.getText().toString().trim();
+    private void userLogin(){
+        String email= editTextEmail.getText().toString().trim();
+        String password= editTextPassword.getText().toString().trim();
 
         if(TextUtils.isEmpty(email)){
             //email is empty
@@ -71,16 +69,15 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         progressDialog.setMessage("Kaydediliyor...");
         progressDialog.show();
 
-        firebaseAuth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+        firebaseAuth.signInWithEmailAndPassword(email,password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
             @Override
             public void onComplete(@NonNull Task<AuthResult> task) {
+                progressDialog.dismiss();
+
                 if(task.isSuccessful()){
-                    //kullanıcı kaydı başarılı.
+                    //activity başlangıcı
                     finish();
                     startActivity(new Intent(getApplicationContext(),ProfileActivity.class));
-                }else {
-                    Toast.makeText(MainActivity.this,"Kayıt başarısız",Toast.LENGTH_SHORT).show();
-                    progressDialog.dismiss();
                 }
             }
         });
@@ -88,11 +85,12 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     @Override
     public void onClick(View view) {
-        if(view == buttonRegister){
-            registerUser();
+        if(view==buttonSignin){
+            userLogin();
         }
-        if(view == textViewSignin){
-            startActivity(new Intent(this,LoginActivity.class));
+        if(view==textViewSignup){
+            finish();
+            startActivity(new Intent(this,MainActivity.class));
         }
     }
 }
